de28823e00b012a3252cb6e97fccb8369659ed06
Kohei Hayakawa <kohei.hayakawa.5@gmail.com>
2015-04-11 23:14:16 +0900

    Change namespace for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  scope module: :api, defaults: { format: 'json' } do
-    namespace :v1 do
-      resources :entries, only: [:index, :show, :create, :update, :destroy]
-      
-    post 'users/sign_up', to: 'users#sign_up'
-    post 'users/sign_in', to: 'users#sign_in'
+  scope defaults: { format: 'json' } do
+    namespace :api do
+      namespace :v1 do
+        resources :entries, only: [:index, :show, :create, :update, :destroy]
+        
+        post 'users/sign_up', to: 'users#sign_up'
+        post 'users/sign_in', to: 'users#sign_in'
+      end
     end
   end
 
